6cd510fa7c044b98c5e3201a3c0739edb5f5b9e5
Chibundu Gift ilechukwu <155324181+chibundu98@users.noreply.github.com>
2025-07-02 03:28:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const addTask = () => {
+    let taskInput = document.getElementById("taskInput");
+    let taskList = document.getElementById("taskList");
+
+    if(taskInput.value === ""){
+        alert("please enter task");
+    }
+
+    let li = document.createElement("li");
+    li.textContent = taskInput.value;
+
+    let deleteButton = document.createElement("span");
+    deleteButton.textContent = "X";
+    deleteButton.className = "delete"
+    deleteButton.onclick = function(){
+        li.remove();
+    }
+    li.appendChild(deleteButton);
+    taskList.appendChild(li)
+
+    taskInput.value = "";
+
+
+
+
+
+}
